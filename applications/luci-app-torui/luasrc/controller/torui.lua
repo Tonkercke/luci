93c9936f7b7d01650f51f93de91ff48dcc09f313
Tonkercke <another2468@protonmail.com>
2022-09-07 18:00:12 +0900

    update 0907

--- applications/luci-app-torui/luasrc/controller/torui.lua
+++ applications/luci-app-torui/luasrc/controller/torui.lua
@@ -1,8 +1,10 @@
 module("luci.controller.torui", package.seeall)
 
 function index()
-	if not nixio.fs.access("/etc/config/torui") then return end
-
-	entry({"admin", "services", "torui"}, cbi("torui"), _("Tor UI"))
-	entry({"admin", "services", "torui", "status"}, call("torui"), nil).leaf = true
+	if not nixio.fs.access("/etc/config/torui") then
+		return
+	end
+	local page
+	page = entry({"admin", "services", "torui"}, cbi("torui"), _("Tor UI"))
+	page.dependent = true
 end
