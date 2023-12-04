0c72981f2f121000de11ae5e8749a4492ae00c8c
Tonkercke <another2468@protonmail.com>
2023-12-04 12:25:40 +0900

    update 23-1204

--- applications/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
+++ applications/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
@@ -1,24 +1,16 @@
-module("luci.controller.qbittorrent", package.seeall)
+module("luci.controller.qbittorrent",package.seeall)
 
 function index()
-	if not nixio.fs.access("/etc/config/qbittorrent") then
-		return
-	end
-
-	entry({"admin", "services", "qbittorrent"}, cbi("qbittorrent"), _("qBittorrent"), 20).dependent = true
-	entry({"admin", "services", "qbittorrent_status"}, call("qbittorrent_status"))
+  if not nixio.fs.access("/etc/config/qbittorrent")then
+    return
+  end
+  entry({"admin","nas","qBittorrent"},cbi("qbittorrent"),_("qBittorrent")).acl_depends = { "luci-app-qbittorrent" }
+  entry({"admin","nas","qBittorrent","status"},call("act_status")).leaf=true
 end
 
-function qbittorrent_status()
-	local sys  = require "luci.sys"
-	local uci  = require "luci.model.uci".cursor()
-	local port = tonumber(uci:get_first("qbittorrent", "qbittorrent", "port"))
-
-	local status = {
-		running = (sys.call("pidof qbittorrent-nox >/dev/null") == 0),
-		port = (port or 8080)
-	}
-
-	luci.http.prepare_content("application/json")
-	luci.http.write_json(status)
+function act_status()
+  local e={}
+  e.running=luci.sys.call("pgrep qbittorrent-nox >/dev/null")==0
+  luci.http.prepare_content("application/json")
+  luci.http.write_json(e)
 end
